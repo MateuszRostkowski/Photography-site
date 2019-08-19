40027f88d29f594d590993fe339eb06d3b657890
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-19 20:47:16 +0200

    Added: responsive styles, interactivity to navbar

--- app.js
+++ app.js
@@ -135,10 +135,65 @@ function init() {
         nextSlide(scrollSlide);
         switchDots(scrollSlide);
         console.log(scrollSlide)
-    }
+    };
 
+    const button = document.querySelector(".menu");
+    const hamburgerLines = document.querySelectorAll(".menu line");
+    const navOpen = document.querySelector(".nav-open");
+    const contact = document.querySelector(".contact");
+    const social = document.querySelector(".social");
+    const logo = document.querySelector(".logo");
 
+    const tl = new TimelineMax({ paused: true, reversed: true });
 
+    tl.to(
+        navOpen,
+        0.5, { y: 0 })
+        .fromTo(
+            contact,
+            0.5,
+            { opacity: 0, y: 10 },
+            { opacity: 1, y: 0 },
+            "-=0.1"
+        )
+        .fromTo(
+            social,
+            0.5,
+            { opacity: 0, y: 10 },
+            { opacity: 1, y: 0 },
+            "-=0.5"
+        )
+        .fromTo(
+            logo,
+            0.3,
+            { color: "white" },
+            { color: "black" },
+            "-=1"
+        )
+        .fromTo(
+            hamburgerLines,
+            0.2,
+            { stroke: "white" },
+            { stroke: "black" },
+            "-=1"
+        )
+        .fromTo(
+            navOpen,
+            0.3,
+            {display: "none"},
+            {display: "grid"},
+            "-=0.5"
+        )
+
+    console.log(button);
+    button.addEventListener("click", function() {
+        tl.reversed() ? tl.play() : tl.reverse();
+    });
+    button.addEventListener("click", function () {
+        console.log("XD");
+    });
+
+    console.log(button);
 }
 
 function throttle(func, limit) {
@@ -152,27 +207,7 @@ function throttle(func, limit) {
       setTimeout(() => (inThrottle = false), limit);
     }
   };
-
-
 }
 
-function nav() {
-    const hamburger = document.querySelector('menu');
-    const hamburgerLines = document.querySelectorAll('.menu line');
-    const navOpen = document.querySelector('.nav-open');
-    const contact = document.querySelector('.contact');
-    const social = document.querySelector('.social');
-    const logo = document.querySelector('.logo');
-    // const = document.querySelector('');
 
-    const tl = new TimelineMax({paused: true});
-    tl.to(navOpen, 0.5, {y:0})
-        .fromTo(
-            concat,
-            0.5,
-            {opacity: 0, y: 10},
-            {opacity: 1, y: 0})
-};
-
-nav();
 init();
